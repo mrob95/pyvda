a7e1f2f7a0eaa0030f9e473adaf7b7312bc08f97
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2021-04-04 21:02:16 +0100

    Disable version check for docs

--- pyvda/__init__.py
+++ pyvda/__init__.py
@@ -32,6 +32,7 @@ Example
 """
 
 import platform
+import os
 
 def _check_version():
     if platform.system() != "Windows" or platform.release() != "10":
@@ -39,7 +40,8 @@ def _check_version():
             "The virtual desktop feature is only available on Windows 10"
         )
 
-_check_version()
+if not os.getenv("READTHEDOCS"):
+    _check_version()
 
 from .app_view import AppView, get_apps_by_z_order
 from .desktop import VirtualDesktop, get_virtual_desktops
